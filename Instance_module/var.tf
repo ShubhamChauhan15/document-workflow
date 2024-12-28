c727c3b6c73ad99dcfb1ec4ead06518a2cc141d0
Shubham Chauhan <shubham.chauhan@ba.com>
2024-12-28 10:20:06 +0530

    Terraform files

---
+++ Instance_module/var.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
---
+++ VPC_module/var.tf
@@ -0,0 +1,31 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "az_1" {
+  description = "The availability zone for the public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az_2" {
+  description = "The availability zone for the private subnet"
+  type        = string
+  default     = "us-east-1b"
+}
+
+
