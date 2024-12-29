b6434670a6a5e73d633faaf4a6df5c545815538a
ShubhamChauhan15 <shubham.chauhan@ba.com>
2024-12-29 20:13:26 +0530

    Update main.tf

--- VPC_module/main.tf
+++ VPC_module/main.tf
@@ -1,83 +1,80 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "epc_vpc" {
+  cidr_block = var.vpc_cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
-  instance_tenancy = "default"
-
   tags = {
-    Name = "migrate-vpc"
+    Name = "EPC_VPC"
   }
 }
-resource "aws_subnet" "pub_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr
-  availability_zone = var.az_1
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.epc_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.public_subnet_az
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet"
+    Name = "Public Subnet"
   }
 }
-resource "aws_subnet" "priv_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr
-  availability_zone = var.az_2
-  map_public_ip_on_launch = false
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.epc_vpc.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.private_subnet_az
   tags = {
-    Name = "private-subnet"
+    Name = "Private Subnet"
   }
 }
-resource "aws_internet_gateway" "IGW" {
-  vpc_id = aws_vpc.main.id
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.epc_vpc.id
   tags = {
-    Name = "main-igw"
-  }
-}
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "public-route-table"
+    Name = "Internet Gateway"
   }
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_eip" "nat" {
+resource "aws_eip" "nat_eip" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-
-  tags = {
-    Name = "main-nat-gateway"
-  }
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  depends_on    = [aws_internet_gateway.internet_gateway]
 }
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.epc_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
-
   tags = {
-    Name = "private-route-table"
+    Name = "Public Route Table"
   }
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.epc_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private-sg"
+  description = "Security group for EC2 instance in private subnet"
+  vpc_id      = aws_vpc.epc_vpc.id
 }
