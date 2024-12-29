746bc949f9dac265e8405ca863adce3f7d7c415c
ShubhamChauhan15 <shubham.chauhan@ba.com>
2024-12-29 21:59:25 +0530

    Update main.tf

--- Instance_module/main.tf
+++ Instance_module/main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "private_instance" {
   }
 }
 
-resource "aws_iam_instance_profile" "ec2_ssm_instance_profile" {
+resource "aws_iam_instance_profile" "ec2_ssm_instance_profile_ecp" {
   name = "ec2-ssm-instance-profile"
   role = aws_iam_role.ec2_ssm_role.name
 }
