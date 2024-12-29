6e2fa6dfe47bb4daec7ab0d017524ad90b047ada
ShubhamChauhan15 <shubham.chauhan@ba.com>
2024-12-29 20:12:42 +0530

    Update var.tf

--- Instance_module/var.tf
+++ Instance_module/var.tf
@@ -1,15 +1,25 @@
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "ID of the private subnet"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group to associate with the EC2 instance"
+  type        = string
+}
+
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The EC2 instance type"
+  description = "The type of EC2 instance"
   type        = string
-  default     = "t3.micro"
-}
-
-variable "region" {
-  description = "The AWS region"
-  type        = string
+  default     = "t2.micro"
 }
