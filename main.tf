fc330ba7d47949b6ad8473300f53811ff3ab869c
ShubhamChauhan15 <shubham.chauhan@ba.com>
2025-01-20 12:39:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,7 @@ module "VPC-MODULE" {
   vpc_cidr_block       = "10.0.0.0/16"
   public_subnet_cidr   = "10.0.1.0/24"
   private_subnet_cidr  = "10.0.2.0/24"
-  public_subnet_az     = "eu-west-2a"
-  private_subnet_az    = "eu-west-2b"
+  private_subnet_az    = "us-east-1a"
 }
 
 module "Instance_module" {
